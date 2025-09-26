fbc58a53398a42691d02167ee1d6ac56db931dba
hung2388 <vusinhhung238@gmail.com>
2025-09-26 08:39:44 +0700

    Add files via upload

---
+++ simplestring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string first,last;
+    cout<<"your first name sir:";
+    cin>>first;
+    cout<<"your last name sir:";
+    cin>>last;
+    string full_name=first+" "+last;
+    cout<<"your name is "<<full_name<<endl;
+    return 0;
+}
